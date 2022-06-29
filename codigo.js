a9c1a30d0e266fede5c5f62ffa6fd8c237195a52
Agustina Yañez <yaneza538@gmail.com>
2022-06-28 21:23:44 -0300

    incorporando array

--- codigo.js
+++ codigo.js
@@ -28,32 +28,40 @@ function saludar() {
 saludar()
 
 function preguntar() {
+	const resultadosAnteriores = [];
 	let pregunta = prompt("¿Queres realizar una operacion? \n 1- si \n 2- no")
 	while (pregunta == "si") {
 		alert("¿Que operación deseas realizar?");
-		let operacion = prompt(`1: suma,  2: resta,  3: división,  4: multiplicación, 5: salir`);
+		let operacion = prompt(`1: suma,  2: resta,  3: división,  4: multiplicación, 5: resultados Anteriores, 6: salir`);
 
 		if (operacion == 1) {
 			let numero1 = prompt("primer número para sumar");
 			let numero2 = prompt("segundo número para sumar");
 			resultado = suma(numero1, numero2);
 			alert(`tu resultado es ${resultado}`);
+			resultadosAnteriores.push(`${resultado}`);
+
 		} else if (operacion == 2) {
 			let numero1 = prompt("primer número para restar");
 			let numero2 = prompt("segundo número para restar");
 			resultado = resta(numero1, numero2);
 			alert(`tu resultado es ${resultado}`);
+			resultadosAnteriores.push(`${resultado}`);
 		} else if (operacion == 3) {
 			let numero1 = prompt("primer número para dividir");
 			let numero2 = prompt("dividido por");
 			resultado = division(numero1, numero2);
 			alert(`tu cociente es ${resultado}`);
+			resultadosAnteriores.push(`${resultado}`);
 		} else if (operacion == 4) {
 			let numero1 = prompt("primer número para multiplicar");
 			let numero2 = prompt("segundo número para multiplicar");
 			resultado = multiplicacion(numero1, numero2);
 			alert(`tu resultado es ${resultado}`);
+			resultadosAnteriores.push(`${resultado}`);
 		}else if (operacion == 5){
+			alert(resultadosAnteriores.join("\n"));;
+		}else if (operacion == 6){
 			break;
 		} else {
 			alert("no se ha encontrado la operación")
